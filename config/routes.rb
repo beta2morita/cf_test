193a77ec70f2eb3dd9a17557f2b13c10bab3afac
Markus Lehmann <markus.lehmann@ibb-bet.de>
2016-09-08 16:44:28 +0200

    4.3 Added orders-controller with empty views

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'static_pages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'static_pages#index'
+ root 'static_pages#index'
+
+
+	resources :orders, only: [:index, :show, :create, :destroy]
+
 end
 
