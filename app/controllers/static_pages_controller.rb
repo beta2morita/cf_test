069ee9377ac8338df63923806356e56ee5241266
Markus Lehmann <markus.lehmann@ibb-bet.de>
2016-10-03 11:07:04 +0200

    5.3 contact form with mail functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,14 @@ class StaticPagesController < ApplicationController
 		@products = Product.where(color: ["red", "blue", "black", "orange", "white"].sample).limit(4)
 	end
 
+	def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  ActionMailer::Base.mail(:from => @email,
+	      :to => 'your-email@example.com',
+	      :subject => "A new contact form message from #{@name}",
+	      :body => @message).deliver_now
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/featured'
 
+  post 'static_pages/thank_you'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- root 'static_pages#featured'
+ 	root 'static_pages#featured'
 
 
 	resources :orders, only: [:index, :show, :create, :destroy]
