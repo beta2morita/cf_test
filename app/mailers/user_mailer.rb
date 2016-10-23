82fd7d1eb474fc8e601e829884528f62b7b1e850
Markus Lehmann <markus.lehmann@ibb-bet.de>
2016-10-23 16:17:11 +0200

    5.11 add user registration email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
         :to => 'markus.lehmann@ibb-bet.de',
         :subject => "A new contact form message from #{name}")
   end
+
+	def welcome(user)
+	  @appname = "Bike Shop"
+	  mail( :to => user.email,
+	        :subject => "Welcome to #{@appname}!")
+	end
+  
 end
