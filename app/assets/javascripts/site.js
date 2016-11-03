fff8f6d84025f3e4a796f266838412c6cb01dad0
Markus Lehmann <markus.lehmann@ibb-bet.de>
2016-11-03 22:04:45 +0100

    6.10 using ajax for comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,15 @@
-$(document).on('turbolinks:load', function(){
-  $('.rating').raty( { path: '/images/ratyrate', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/images/ratyrate',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
+var refreshRating = function() {
+    $('.rating').raty( { path: '/images/ratyrate', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/images/ratyrate',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+        }
+    });
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+  refreshRating();
   $(".img-zoom").elevateZoom({
     zoomType: "lens",
     lensShape: "round",
